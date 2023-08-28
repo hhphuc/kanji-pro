26e9138953d3bf3a4a64d43040443c0ee31b75b3
Stark <stark.huynh@sourcecode.com.au>
2023-08-28 08:21:37 +0700

    Updates

---
+++ precache-manifest.c87071edb91ff46b30688d9dae65d609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d93064ba7ef0deedbd5f8f11cb8ecb",
+    "url": "/kanji-pro/index.html"
+  },
+  {
+    "revision": "8e6c836aa2cd8523be82",
+    "url": "/kanji-pro/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d6204637f27566a5a751",
+    "url": "/kanji-pro/static/js/2.ca09a3e9.chunk.js"
+  },
+  {
+    "revision": "8e6c836aa2cd8523be82",
+    "url": "/kanji-pro/static/js/main.e3d1ff3d.chunk.js"
+  },
+  {
+    "revision": "1ffb721212b25b8f224d",
+    "url": "/kanji-pro/static/js/runtime~main.c00cc48e.js"
+  }
+]);
